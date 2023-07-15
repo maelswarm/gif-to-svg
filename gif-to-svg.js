677816faa80bede6c7c6fb6639b22e9bae90df10
maelswarm <roecrew@gmail.com>
2023-07-15 12:18:25 -0400

    0.1.2

--- gif-to-svg.js
+++ gif-to-svg.js
@@ -6,21 +6,7 @@ const sizeOf = require('image-size');
 const extractFrames = require('gif-extract-frames');
 const filePath = process.argv[2];
 const rate = Number(process.argv[3]) || 0.1;
-let svg = `<svg xmlns="http://www.w3.org/2000/svg"
-xmlns:xlink="http://www.w3.org/1999/xlink">
-<style>
-@keyframes anim {
-0% {
-   opacity: 0;
-}
-49% { opacity: 0; }
-50% {
-   opacity: 1;
-}
-}
-image {
-opacity: 0;
-}`;
+let svg = ``;
 
 const dir = __dirname + '/frames';
 if(fs.existsSync(dir)) {
@@ -33,17 +19,33 @@ extractFrames({
     output: './frames/frame-%d.png'
 }).then(() => {
     fs.readdir(dir, async (err, files) => {
-        //handling error
         if (err) {
             return console.log('Unable to scan directory: ' + err);
         }
-    
+        svg += `<svg xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink">
+        <style>
+        @keyframes anim {
+        0% { opacity: 1; }
+        ${100 / files.length}% {
+           opacity: 1;
+        }
+        ${(100 / files.length) + 0.001}% {
+            opacity: 0;
+        }
+        100% {opacity: 0;}
+        }
+        image {
+        opacity: 0;
+        }`;
+        files = files.sort((a, b) => a.localeCompare(b, undefined, { numeric: true }));
         for(let i =0; i<files.length; ++i) {
             const dim = sizeOf(path.join(__dirname, 'frames/' + files[i]));
+            console.log(files[i])
             const dataURI = await imageDataURI.encodeFromFile(path.join(__dirname, 'frames/' + files[i]));
             const svgImageStyle = `
                 image#img${i} {
-                  animation: anim ${(files.length * rate).toFixed(2)}s linear infinite;
+                  animation: anim ${(rate * files.length).toFixed(2)}s linear infinite;
                   animation-delay: ${(i * rate).toFixed(2)}s;
                 }
                 `;
